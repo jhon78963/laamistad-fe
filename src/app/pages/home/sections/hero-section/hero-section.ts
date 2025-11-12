eeef9f486d70385ecf0691d00ebd3278797be273
Jhon Livias <jhonlivias3@gmail.com>
2025-11-11 23:42:03 -0500

    fix reservation cta

--- src/app/pages/home/sections/hero-section/hero-section.ts
+++ src/app/pages/home/sections/hero-section/hero-section.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
