e1ae6d85113310925bdd83b248f38f131e0eedad
Jhon Livias <jhonlivias3@gmail.com>
2025-11-12 00:50:08 -0500

    update

--- src/app/pages/home/sections/hero-section/hero-section.ts
+++ src/app/pages/home/sections/hero-section/hero-section.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
